b0e9c4ec9cd8f3bc93e6fffb43ed9d6286046196
kabessC <chenjunkai1024@outlook.com>
2015-11-24 18:29:51 +0800

    Add preview

--- static/js/upload.js
+++ static/js/upload.js
@@ -53,7 +53,7 @@
 				}
 			}, false);
 
-			xhr.addEventListener("load", function(){
+			xhr.addEventListener("load", function(response){
 				if( (xhr.status >= 200 && xhr.status < 300) || xhr.status == 304 )
 				{
 					var successMsg = document.createElement('span');
@@ -61,6 +61,12 @@
 					successMsg.appendChild(text);
 					progressmsg.appendChild(successMsg);
 					func();
+					if(response == 'True'){
+						var link = document.createElement("a");
+						link.setAttribute("target", "_blank");
+						link.setAttribute("href", "/preview");
+						link.click();
+					}
 				}
 				else
 				{
