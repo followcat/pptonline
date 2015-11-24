4204e08e0ee320acfc3c224febcc1cc1695e6a16
followcat <followcat@163.com>
2015-11-24 18:09:29 +0800

    Add preview and save convert stream.

--- main.py
+++ main.py
@@ -4,19 +4,30 @@ import flask
 from flask import Flask
 app = Flask(__name__)
 
+
+save_converted = ""
+
+
 @app.route('/')
 def home():
     return flask.render_template("upload.html")
 
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if flask.request.method == 'POST':
+        global save_converted
         network_file = flask.request.files['file']
         stream = network_file.read()
-        output = pypandoc.convert(stream, 'revealjs', format='md',
-                                  extra_args=['--standalone',
-                                              '--self-contained'])
-        return output
+        save_converted = pypandoc.convert(stream, 'revealjs', format='md',
+                                          extra_args=['--standalone',
+                                                      '--self-contained'])
+        return "True"
+
+
+@app.route('/preview', methods=['GET'])
+def preview():
+    return save_converted
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
