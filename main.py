e7dd377098f74819c4693b6d43469b9472ba8519
followcat <followcat@163.com>
2015-12-07 18:31:06 +0800

    Add charset to convert different charset.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import charset
 import pypandoc
 
 import flask
 from flask import Flask
+from flask import request, send_from_directory
 app = Flask(__name__)
 
 
@@ -13,13 +15,14 @@ def home():
     return flask.render_template("upload.html")
 
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload():
     if flask.request.method == 'POST':
         global save_converted
         network_file = flask.request.files['file']
         stream = network_file.read()
-        save_converted = pypandoc.convert(stream, 'revealjs', format='md',
+        utf8_stream = charset.text_to_utf8(stream)
+        save_converted = pypandoc.convert(utf8_stream, 'revealjs', format='md',
                                           extra_args=['--standalone',
                                                       '--self-contained'])
         return "True"
@@ -29,5 +32,6 @@ def upload():
 def preview():
     return save_converted
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
